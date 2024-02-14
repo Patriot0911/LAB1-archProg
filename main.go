b75e0770b757e01957fb9760a206bba3c016571f
Ivan Pasternak <retriumpf@gmail.com>
2024-02-14 17:23:20 +0200

    Added part of init

--- main.go
+++ main.go
@@ -14,6 +14,14 @@ const (
 	PORT = ":8795"
 )
 
+func init() {
+  http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+    timestamp := Timestamp{time.Now().Format(time.RFC3339)}
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(timestamp)
+  })
+}
+
 func (t Timestamp) String() string {
 	return t.Value[:19]
 }
