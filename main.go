d9937a650aa2d30cacfdc5e416c3518c4ce351b2
#Patriot0911 <75264092+Patriot0911@users.noreply.github.com>
2024-02-14 18:48:06 +0200

    35

--- main.go
+++ main.go
@@ -1,34 +1,3 @@
-package main
-
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-type Timestamp struct {
-	Value string `json:"time"`
-}
-
-const (
-	PORT = ":8795"
-)
-
-func init() {
-  http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-    timestamp := Timestamp{time.Now().Format(time.RFC3339)}
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(timestamp)
-  })
-}
-
-func (t Timestamp) String() string {
-	return t.Value[:19]
-}
-
-/// TTOTOTOTOTOT
-
-func main() {
-	fmt.Println("Server is listening on port", PORT)
-	log.Fatal(http.ListenAndServe(PORT, nil))
-}
+1
+2
+3
